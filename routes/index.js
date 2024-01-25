c6344a1e8bdc1c261c097c0b7f12a5a97a865d5d
elmambou <mam13003@byui.edu>
2024-01-25 22:52:56 +0200

    additional changes to lesson2 assignment

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
- 
-routes.get('/', lesson1Controller.emilyRoute);
-routes.get('/William', lesson1Controller.hannahRoute);
-routes.get('/Chante', lesson1Controller.ChanteMooreRoute);
-module.exports = routes;
+const express = require('express');
+const router = express.Router();
+
+router.use('/contacts', require('./contacts'))
+
+module.exports = router;
