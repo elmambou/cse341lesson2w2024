bda2158d04c35394338b9a33301fc575ddde4c50
elmambou <mam13003@byui.edu>
2024-01-25 17:32:24 +0200

    adding new routes to lesson1

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -1,12 +1,17 @@
 const emilyRoute = (req, res) => {
     res.send("Sarah Annah Birch");
   };
-  
+
+const ChanteMooreRoute = (req, res) => {
+    res.send("Chante Moore");
+  };
+    
 const hannahRoute = ((req, res) => {
     res.send("William Birch");
     });
 
 module.exports = {
     emilyRoute,
-    hannahRoute
+    hannahRoute,
+    ChanteMooreRoute
 }
